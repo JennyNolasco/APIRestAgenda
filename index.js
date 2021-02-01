7eebc1062ee1e3c0e36c3a3764f503448abce399
Jeniffer <jenifferferreira@brq.com>
2021-01-31 22:46:07 -0300

    Incluido nodemon

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ const app = express()
 app.listen(3000,  () => {
     console.log('Servidor Rodando na porta 3000')
 });
+
+// O express nos fornece 2 coisas: req e resp
+// Req o que estamos recebendo e Resp o que iremos enviar 
+app.get('/', (req, resp) => {
+    resp.send('Servidor OK')
+});
